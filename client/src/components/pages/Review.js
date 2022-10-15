19bd94d345b1bdc8f5775e0ee68df1d7686d7d86
tgallagher496 <tgallagher496@gmail.com>
2022-10-15 13:59:23 -0400

    Nav Bar Skeleton

---
+++ client/src/components/pages/Review.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Review() {
+  return (
+    <div>
+      <h1>Review Page</h1>
+      <p>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+            <div className="card" style= {{width: '18rem'}}>
+            </div>
+            <div class="col-sm">
+              <p>
+              This is our Review page.
+              </p>
+            </div>
+            </div>
+          </div>
+        </div>
+      </p>
+    </div>
+  );
+}

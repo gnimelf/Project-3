465e519178c9c267b33c530c15e1a4a2a7e893bf
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-10-19 22:02:12 -0400

    install bcrypt && jsonwebtoken

--- client/src/components/pages/LogIn.js
+++ client/src/components/pages/LogIn.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from 'react-bootstrap';
 
-// import { LOGIN_USER } from "../utils/mutations";
-// import Auth from '../utils/auth';
+// import { LOGIN_USER } from "../";
+// import Auth from '';
+
 
 const LogIn = () => {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });

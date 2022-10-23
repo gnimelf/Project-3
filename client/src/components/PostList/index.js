12e10f977883cfcbfa97ca043e426816c646e2ca
tgallagher496 <tgallagher496@gmail.com>
2022-10-23 13:19:34 -0400

    PostList

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const PostList = ({ posts }) => {
+  console.log(posts.length);
   if (!posts.length) {
     return <h3>No posts Yet</h3>;
   }
